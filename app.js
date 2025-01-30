de34c6c67592f6049829d83d37e18b40164dffc4
Katherine German <katherine03gt@gmail.com>
2025-01-30 19:24:29 -0400

    adding function to add names

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//empty variable type array to save values 
+let nombres = [ ];
+
+//function to add friends name
+function agregarAmigo() {
+   // let inputName = document.querySelector(".input-name").value;
+    
+   // get values from input entry 
+   let inputName = document.querySelector(".input-name").value;
+
+
+   
+   //let nuevosNombres = nombres.push('')
+    
+}
+
